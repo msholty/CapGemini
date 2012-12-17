820160252fb3b8ac899d533bc01be78b20cc4c72
msholty <drawman@gmail.com>
2012-12-17 15:44:01 -0500

    testing bootstrap

--- public/index.php
+++ public/index.php
@@ -1,14 +1,13 @@
 <?php
 
-echo 'test1';
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
-
+echo 'test6';
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-
+    echo 'test10';
 // Ensure library/ is on include_path
 set_include_path(
 	implode(
@@ -21,15 +20,17 @@ set_include_path(
 		)
 	)
 );
-
+echo 'test23';
 /** Zend_Application */
 require_once 'Zend/Application.php';
-
+echo 'test26';
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
 );
+echo 'test32';
 $application->bootstrap()
             ->run();
+echo 'test35';
 ?>
