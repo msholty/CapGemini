168d8f931a0ad1b0febfa8e1684878348287af4f
msholty <drawman@gmail.com>
2012-12-17 15:50:29 -0500

    testing again

--- public/index.php
+++ public/index.php
@@ -1,13 +1,11 @@
 <?php
-
+echo realpath(dirname(__FILE__) . '/../application');
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
-echo 'test6';
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-    echo 'test10';
 // Ensure library/ is on include_path
 set_include_path(
 	implode(
@@ -20,17 +18,15 @@ set_include_path(
 		)
 	)
 );
-echo 'test23';
 /** Zend_Application */
 require_once 'Zend/Application.php';
-echo 'test26';
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
 );
-echo 'test32';
+echo '$application->bootstrap()->run()';
 $application->bootstrap()
             ->run();
-echo 'test35';
+echo 'done';
 ?>
