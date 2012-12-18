c422439808ccf8d820910e9ca063afa91e94d14f
msholty <drawman@gmail.com>
2012-12-18 09:42:27 -0500

    index.php test for new container

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,66 +2,86 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    protected function _initDoctype()
-    {
-        $this->bootstrap('view');
-        $view = $this->getResource('view');
-        $view->doctype('XHTML1_STRICT');
-    }
-
-    /**
-    * Set up the view helpers to use the "Default" namespace.
-    * @return null
-    */
-    protected function _initDefaultViewHelpers()
-    {
-        /* @var $view Zend_View */
-        $view = $this->bootstrap('view')->getResource('view');
-        $view->addHelperPath(
-            APPLICATION_PATH . '/views/helpers',
-            'Application_View_Helper_'
-        );
-    }
+	protected function _initDoctype()
+	{
+		/*$this->bootstrap('view');
+		$view = $this->getResource('view');
+		$view->doctype('XHTML1_STRICT');*/
+	}
 
 	/**
-     * Init default controller action helpers
-     */
-    protected function _initDefaultControllerActionHelpers()
-    {
-        Zend_Controller_Action_HelperBroker::addPath('controllers/helpers');
-    }
+	 * Initializes the Autoloader
+	 */
+	protected function _initAutoload()
+	{
+		$autoloader = new Zend_Application_Module_Autoloader(
+				array(
+						'namespace' => 'Default',
+						'basePath'  => '/.apps/http/__default__/0/1.0-zdc/application',
+						'resourceTypes' => array(
+								'module' => array(
+										'namespace' => 'module_',
+										'path' => 'modules/'
+								),
+						)
+				)
+		);
+		return $autoloader;
+	}
 
-    protected function _initRouter()
-    {
-        // Get Front Controller Instance
-        $front = Zend_Controller_Front::getInstance();
+	/**
+	 * Set up the view helpers to use the "Default" namespace.
+	 * @return null
+	 */
+	protected function _initDefaultViewHelpers()
+	{
+		/* @var $view Zend_View */
+		$view = $this->bootstrap('view')->getResource('view');
+		$view->addHelperPath(
+				APPLICATION_PATH . '/views/helpers',
+				'Application_View_Helper_'
+		);
+	}
 
-        // Get Router
-        $router = $front->getRouter();
+	/**
+	 * Init default controller action helpers
+	 */
+	protected function _initDefaultControllerActionHelpers()
+	{
+		Zend_Controller_Action_HelperBroker::addPath('controllers/helpers');
+	}
 
-        //id = resource._id in database
-        $route = new Zend_Controller_Router_Route(
-                'resources/page/:page',
-                array(
-                		'module'	 => 'default',
-                        'controller' => 'resources',
-                        'action'     => 'index'
-                )
-        );
+	protected function _initRouter()
+	{
+		// Get Front Controller Instance
+		$front = Zend_Controller_Front::getInstance();
 
-        $router->addRoute('resources', $route);
+		// Get Router
+		$router = $front->getRouter();
 
-        //id = project.id in database
-        $route = new Zend_Controller_Router_Route(
-        		'projects/page/:page',
-        		array(
-        				'module'	 => 'default',
-        				'controller' => 'projects',
-        				'action'     => 'index'
-        		)
-        );
+		//id = resource._id in database
+		$route = new Zend_Controller_Router_Route(
+				'resources/page/:page',
+				array(
+						'module'	 => 'default',
+						'controller' => 'resources',
+						'action'     => 'index'
+				)
+		);
 
-        $router->addRoute('projects', $route);
-    }
+		$router->addRoute('resources', $route);
+
+		//id = project.id in database
+		$route = new Zend_Controller_Router_Route(
+				'projects/page/:page',
+				array(
+						'module'	 => 'default',
+						'controller' => 'projects',
+						'action'     => 'index'
+				)
+		);
+
+		$router->addRoute('projects', $route);
+	}
 }
 
