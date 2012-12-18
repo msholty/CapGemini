a808d2f35607f2e2e68f419404e6e9392a2d2667
msholty <drawman@gmail.com>
2012-12-18 10:32:16 -0500

    htaccess fix omg

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-echo 'test';
+//echo 'test';
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
